c8403c29ced54bb9e4fb0123f21c773220efce46
Dean Johnson <johnson.kennethdean@gmail.com>
2019-02-11 12:31:06 -0800

    Add navbar

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// Setup for CSS, JS Enqueue
 function university_files(){
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime() , true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -10,6 +11,9 @@ function university_files(){
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features(){
+  register_nav_menu('headerMenuLocation','Header Menu Location');
+  register_nav_menu('footerLocationOne','Footer Location One');
+  register_nav_menu('footerLocationTwo','Footer Location Two');
   add_theme_support('title-tag');
 }
 

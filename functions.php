a5d554a9d0e8ad68e014be9a9d1be5d008063ed0
Dean Johnson <johnson.kennethdean@gmail.com>
2019-02-19 11:50:39 -0800

    Add Event and Archive page for events

--- functions.php
+++ functions.php
@@ -21,8 +21,11 @@ add_action('after_setup_theme', 'university_features');
 
 function university_post_types(){
   register_post_type('event', array(
-    'public'  =>  true,
-    'labels'  => array(
+    'supports'          => array('title','editor','excerpt'),
+    'rewrite'           =>  array('slug' => 'events'),
+    'has_archive'       =>  true,
+    'public'            =>  true,
+    'labels'            =>  array(
       'name'            =>  'Events',
       'add_new_items'   =>  'Add New Event',
       'edit_item'       =>  'Edit Event',

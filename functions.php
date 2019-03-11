95caedd552cdfbaa8c38ec90a634fdd7c69d311a
Thrillhause <johnson.kennethdean@gmail.com>
2019-03-10 17:22:32 -0700

    Add professor types

--- functions.php
+++ functions.php
@@ -51,6 +51,20 @@ function university_post_types(){
     ),
     'menu_icon' =>  'dashicons-awards'
   ));
+
+    // Professor Post Type
+    register_post_type('professor', array(
+      'supports'          => array('title','editor'),
+      'public'            =>  true,
+      'labels'            =>  array(
+        'name'            =>  'Professors',
+        'add_new_items'   =>  'Add New Professor',
+        'edit_item'       =>  'Edit Professor',
+        'all_items'       =>  'All Professors',
+        'singular_name'   =>  'Program'
+      ),
+      'menu_icon' =>  'dashicons-welcome-learn-more'
+    ));
 }
 
 add_action('init', 'university_post_types');

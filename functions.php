8611031326e2415fbac03637c610ec71863efda8
Thrillhause <johnson.kennethdean@gmail.com>
2019-03-21 11:27:47 -0700

    Add pageBanner support. Start function for pageBanner. Current 11-44

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
 
+function pageBanner($args) {
+  
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  ?>
+    <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php the_field('page_banner_subtitle'); ?></p>
+      </div>
+    </div>  
+  </div>
+<?php }
+
 // Setup for CSS, JS Enqueue
 function university_files(){
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime() , true);
@@ -16,8 +34,9 @@ function university_features(){
   // register_nav_menu('footerLocationTwo','Footer Location Two');
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
-  add_image_size('professorLandscape',400 ,260, true);
+  add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');

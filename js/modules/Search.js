0851549bd26ca3a23b33e517c7b6e3b38aff8860
Thrillhause <johnson.kennethdean@gmail.com>
2019-03-28 21:02:29 -0700

    Add typinglogic and timeout functions. 13.55

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -5,16 +5,25 @@ class Search {
 		this.openButton = $('.js-search-trigger');
 		this.closeButton = $('.search-overlay__close');
 		this.searchOverlay = $('.search-overlay');
+		this.searchField = $('#search-term');
 		this.isOverlayOpen = false;
 		this.events();
+		this.typingTimer;
 	}
 
 	events() {
 		this.openButton.on('click', this.openOverlay.bind(this));
 		this.closeButton.on('click', this.closeOverlay.bind(this));
-		$(document).on('keyup', this.keyPressDispatcher.bind(this));
+		$(document).on('keydown', this.keyPressDispatcher.bind(this));
+		this.searchField.on('keydown', this.typingLogic.bind(this));
 	}
 
+	typingLogic() {
+		clearTimeout(this.typingTimer);
+		this.typingTimer = setTimeout(function() {
+			console.log('Testingtimer');
+		}, 2000);
+	}
 	openOverlay() {
 		this.searchOverlay.addClass('search-overlay--active');
 		$('body').addClass('body-no-scroll');

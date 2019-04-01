9cc8918a8773d2602749ed34674134eab632f7ad
Dean Johnson <johnson.kennethdean@gmail.com>
2019-04-01 11:17:25 -0700

    Add WP Rest API Search. Update getResults. 14.58

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -40,8 +40,14 @@ class Search {
 		this.previousValue = this.searchField.val();
 	}
 	getResults() {
-		this.resultsDiv.html('Imagine real search results here...');
-		this.isSpinnerVisible = false;
+		$.getJSON('http://thrillhausework.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+			this.resultsDiv.html(`
+			<h2 class="search-overlay__section-title">General Information</h2>
+			<uln class="link-list min-list">
+				${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+			</ul>
+			`);		
+		})
 	}
 	openOverlay() {
 		this.searchOverlay.addClass('search-overlay--active');

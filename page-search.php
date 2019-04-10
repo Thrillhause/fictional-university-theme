b934c2e77ae88c6fcbb4f21eacdd851e134b94db
Dean Johnson <johnson.kennethdean@gmail.com>
2019-04-10 10:55:02 -0700

    Update submit type 17.73

--- page-search.php
+++ page-search.php
@@ -51,7 +51,7 @@
         <label class="headline headline--medium" for="s">Perform a New Search</label>
         <div class="search-form-row">
             <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
-            <input class="search-submit"type="search" value="Search">  
+            <input class="search-submit" type="submit" value="Search">  
         </div>
       </form>
     </div>

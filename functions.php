ee7879abe3d66973baa75ca09dfa0125c894dcd6
Dean Johnson <johnson.kennethdean@gmail.com>
2019-04-15 10:53:51 -0700

    Add subscriber registration. Update login form. Update header. 19.77

--- functions.php
+++ functions.php
@@ -178,3 +178,44 @@ function universityMapKey($api){
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+// Redirect Subscriber accounts to homepage
+
+add_action('admin_init','noSubsAdminBar');
+
+function redirectSubsToFrontend(){
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit();
+  }
+}
+
+add_action('wp_loaded','noSubsAdminBar');
+
+function noSubsAdminBar(){
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+// Customize Login Screen
+
+add_filter('login_headerurl','ourHeaderUrl');
+
+function ourHeaderUrl(){
+  return esc_url(site_url('/'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle(){
+  return get_bloginfo('name');
+}
+
+add_action('login_enqueue_scripts','ourLoginCSS');
+
+function ourLoginCSS(){
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri()); 
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}

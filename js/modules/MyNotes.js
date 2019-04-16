6d97073a7bd48f00d28373e63e05a83656b1f1e0
Thrillhause <johnson.kennethdean@gmail.com>
2019-04-16 16:27:16 -0700

    Add editNote and saveNote. 19.82

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -6,44 +6,74 @@ class MyNotes {
 	}
 	events() {
 		$('.delete-note').on('click', this.deleteNote);
-		$('.edit-note').on('click', this.editNote);
+		$('.edit-note').on('click', this.editNote.bind(this));
+		$('.update-note').on('click', this.updateNote.bind(this));
 	}
-	editNote(e){
-		var thisNote = $(e.target).parents("li");
-		thisNote.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field');
+	editNote(e) {
+		var thisNote = $(e.target).parents('li');
+		if (thisNote.data('state') == 'editable') {
+			this.makeNoteReadyOnly(thisNote);
+		} else {
+			this.makeNoteEditable(thisNote);
+		}
+	}
+	makeNoteEditable(thisNote) {
+		thisNote.find('.edit-note').html('<i class="fa fa-times" aria-hidden="true"></i>Cancel');
+		thisNote
+			.find('.note-title-field, .note-body-field')
+			.removeAttr('readonly')
+			.addClass('note-active-field');
 		thisNote.find('.update-note').addClass('update-note--visible');
-		// $.ajax({
-		// 	beforeSend: (xhr)=>{
-		// 		xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
-		// 	},
-		// 	url: universityData.root_url + `/wp-json/wp/v2/note/${thisNote.data('id')}`,
-		// 	type: 'EDIT',
-		// 	success: (response)=>{
-
-		// 		console.log('Updated');
-		// 		console.log(response);
-		// 	},
-		// 	error: (response)=>{
-		// 		console.log('Sorry');
-		// 		console.log(response);
-		// 	}
-		// });
+		thisNote.data('state', 'editable');
+	}
+	makeNoteReadyOnly(thisNote) {
+		thisNote.find('.edit-note').html('<i class="fa fa-pencil" aria-hidden="true"></i>Edit');
+		thisNote
+			.find('.note-title-field, .note-body-field')
+			.attr('readonly', 'readonly')
+			.removeClass('note-active-field');
+		thisNote.find('.update-note').removeClass('update-note--visible');
+		thisNote.data('state', 'cancel');
 	}
 	deleteNote(e) {
-		var thisNote = $(e.target).parents("li");
+		var thisNote = $(e.target).parents('li');
 
 		$.ajax({
-			beforeSend: (xhr)=>{
+			beforeSend: xhr => {
 				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
 			},
 			url: universityData.root_url + `/wp-json/wp/v2/note/${thisNote.data('id')}`,
 			type: 'DELETE',
-			success: (response)=>{
+			success: response => {
 				thisNote.slideUp();
 				console.log('Congrats');
 				console.log(response);
 			},
-			error: (response)=>{
+			error: response => {
+				console.log('Sorry');
+				console.log(response);
+			}
+		});
+	}
+	updateNote(e) {
+		var thisNote = $(e.target).parents('li');
+		var ourUpdatedPost = {
+			title: thisNote.find('.note-title-field').val(),
+			content: thisNote.find('.note-body-field').val()
+		};
+		$.ajax({
+			beforeSend: xhr => {
+				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+			},
+			url: universityData.root_url + `/wp-json/wp/v2/note/${thisNote.data('id')}`,
+			type: 'POST',
+			data: ourUpdatedPost,
+			success: response => {
+				this.makeNoteReadyOnly(thisNote);
+				console.log('Congrats');
+				console.log(response);
+			},
+			error: response => {
 				console.log('Sorry');
 				console.log(response);
 			}

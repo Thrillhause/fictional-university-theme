0244705b0f8c84f76782ec0b784eea32982ff568
Thrillhause <johnson.kennethdean@gmail.com>
2019-04-17 11:27:51 -0700

    Add CRUD api for notes page. 19.84

--- page-my-notes.php
+++ page-my-notes.php
@@ -17,6 +17,14 @@
    
 
     <div class="generic-content">
+    
+      <div class="create-note">
+        <h2 class="headling headling--medium">Create New Note</h2>
+        <input class="new-note-title" placeholder="Title"">
+        <textarea class="new-note-body"placeholder="Your note here..."></textarea>
+        <span class="submit-note">Create Note</span>
+      </div>
+
       <ul class="min-list link-list"id="my-notes">
       <?php 
         $userNotes = new WP_Query(array(

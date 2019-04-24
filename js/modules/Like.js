273398164c5eb74384f79c40cdbde8fd45061d24
Thrillhause <johnson.kennethdean@gmail.com>
2019-04-24 12:18:14 -0700

    Add php like functionality. 20.91

--- js/modules/Like.js
+++ js/modules/Like.js
@@ -11,15 +11,16 @@ class Like {
 		var currentLikeBox = $(e.target).closest('.like-box');
 
 		if (currentLikeBox.data('exists') == 'yes') {
-			this.deleteLike();
+			this.deleteLike(currentLikeBox);
 		} else {
-			this.createLike();
+			this.createLike(currentLikeBox);
 		}
 	}
-	createLike() {
+	createLike(currentLikeBox) {
 		$.ajax({
 			url: universityData.root_url + '/wp-json/university/v1/manageLike',
 			type: 'POST',
+			data: { professorID: currentLikeBox.data('professor') },
 			success: response => {
 				console.log(response);
 			},
@@ -28,7 +29,7 @@ class Like {
 			}
 		});
 	}
-	deleteLike() {
+	deleteLike(currentLikeBox) {
 		$.ajax({
 			url: universityData.root_url + '/wp-json/university/v1/manageLike',
 			type: 'DELETE',
